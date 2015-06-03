a9e75f4e27ebf5a1e76e1d8178530f9b2b618bfe
jonisno <jonis@jonis.no>
2015-06-03 14:13:30 +0000

    Prompt for deletion of old files

--- bin/setupdotfiles.sh
+++ bin/setupdotfiles.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
-DOTDIR="$HOME/dotfiles"
+initialize() {
+	DOTDIR="$HOME/dotfiles"
 
-# symlink needed dotfiles.
+	for i in .{vimrc,bashrc,gitconfig,vim}
+	do
+		if [ ! -f "$HOME/${i}" -a ! -L "$HOME/${i}" ] ; then
+			ln -s $DOTDIR/${i:1} ~/${i}
+		fi
+	done
 
-for i in .{vimrc,bashrc,gitconfig,vim}
-do
-	if [ ! -f "$HOME/${i}" -a ! -L "$HOME/${i}" ] ; then
-		ln -s $DOTDIR/${i:1} ~/${i}
+	# check for bundle
+	BUNDLEDIR="$DOTDIR/vim/bundle"
+	if [ ! -d "$BUNDLEDIR" ] ; then
+		mkdir -p $BUNDLEDIR
+		git clone git@github.com:gmarik/Vundle.vim.git $BUNDLEDIR/Vundle.vim
 	fi
-done
 
-# check for bundle
-BUNDLEDIR="$DOTDIR/vim/bundle"
-if [ ! -d "$BUNDLEDIR" ] ; then
-	mkdir -p $BUNDLEDIR
-	git clone git@github.com:gmarik/Vundle.vim.git $BUNDLEDIR/Vundle.vim
-fi
+	if [ ! -d "$DOTDIR/tmp" ] ; then
+		mkdir -p "$DOTDIR/tmp"
+	fi
 
-if [ ! -d "$DOTDIR/tmp" ] ; then
-	mkdir -p "$DOTDIR/tmp"
-fi
+	. ~/.bashrc
+	vim +BundleInstall +qall
+}
 
-. ~/.bashrc
-vim +BundleInstall +qall
+echo "This will delete .bashrc .vimrc and .gitconfig plus the .vim directory in your $HOME folder."
+read -p "Initialize and delete, y/n? " yn
+case "$yn" in
+	y|Y)
+		echo "Deleting old files."
+		rm $HOME/.bashrc $HOME/.vimrc $HOME/.gitconfig
+		rf -r $HOME/.vim
+		initialize
+	;;
+	n|N)
+		echo "Aborting, bye.."
+		exit 0
+	;;
+	*)
+		echo "Invalid option."
+		exit 0
+esac
